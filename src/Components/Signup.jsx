189f3f61de60538641ee50797ab2ecdcd0cc6d69
tahreem014 <125256534+tahreem014@users.noreply.github.com>
2023-09-30 10:37:49 +0500

    get data 10:37 9-30-2023

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,143 +1,144 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-
- //import firebase
-import {auth } from "../config/firebase"
-import {createUserWithEmailAndPassword} from 'firebase/auth';
-
+//import firebase
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 //import files
-import './signup.css'
+import "./signup.css";
 
-// Router Link 
-import { Link} from 'react-router-dom';
+// Router Link
+import { Link } from "react-router-dom";
 
-// import icons 
-import {FaEyeSlash} from 'react-icons/fa'
-import {FaEye} from 'react-icons/fa'
-
+// import icons
+import { FaEyeSlash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
 
 function Signup() {
-const [open, setOpen]  = useState(false)
+  const [open, setOpen] = useState(false);
 
+  const toggle = () => {
+    setOpen(!open);
+  };
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  console.log("correct");
 
-const toggle=()=>{
-  setOpen(!open)
-}
+  // validation
+  const validation = () => password === confirmPassword;
+  console.log("validate");
 
+  console.log(auth?.currentUser?.email);
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
-  console.log('correct')
+  // Sign Up
+  const signIn = async () => {
+    // e.preventDefault(); // Prevent the form from submitting
+    if (!validation()) {
+      alert("Password not matched");
+      return; // Don't proceed to Home page if password is not matched
+    }
+    // TODO: Handle successful form submission (e.g., navigate to Home page)
+    else {
+    }
 
- 
+    console.log("before");
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+        confirmPassword
+      );
+      const userData = {
+        email,
+        password,
+        confirmPassword,
+      };
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      };
 
-        // validation 
-        const validation = () => password === confirmPassword;
-        console.log('validate')
+      const res = await fetch(
+        "https://reactauth-3db27-default-rtdb.asia-southeast1.firebasedatabase.app/userData.json",
+        options
+      );
+      if (res.ok) {
+        alert("Data store");
+      } else {
+        alert("Error occured");
+      }
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
 
-
-        console.log(auth?.currentUser?.email)
-
-
-        // Sign Up
-        const signIn = async () => {
-            // e.preventDefault(); // Prevent the form from submitting
-                if (!validation()) {
-                  alert('Password not matched');
-                  return; // Don't proceed to Home page if password is not matched
-                }
-                // TODO: Handle successful form submission (e.g., navigate to Home page)
-                else{
-                 
-                }
-
-            console.log('before')
-            try {
-                await createUserWithEmailAndPassword(auth, email, password, confirmPassword);
-                const userData = {
-                  email,
-                  password,
-                  confirmPassword,
-
-            }; 
-            const options = {
-              method: 'POST',
-              headers:{
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(userData),
-            };
-
-            const res = await fetch
-            ('https://reactauth-3db27-default-rtdb.asia-southeast1.firebasedatabase.app/userData.json' ,
-            options)
-            if(res.ok){
-              alert('Data store')
-            } else{
-              alert('Error occured')
-            }
-
-          }
-          catch (err) {
-            console.error(err);
-            alert(err.message);
-        }
-
-
-        };
-                
-                    
-    
-        return (
+  return (
     <div>
       <div className="signup-box">
         <h2>Sign Up</h2>
 
-            <div className="user">
-                <input type="text" name="email" required=''  placeholder='Username'
-                value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div>
-            
-            
-             <span className='icon'> 
-                { open === false? 
-                <FaEye onClick={toggle}/>:
-                <FaEyeSlash onClick={toggle} />}
-                </span>
-                <div className="user ">
-                <input type={open === false? 'password': 'text'}
-                 name="password" required=" " placeholder='Password' 
-                 value={password} onChange={(e) => setPassword( e.target.value )}
-                 />
-            </div>
-                
+        <div className="user">
+          <input
+            type="text"
+            name="email"
+            required=""
+            placeholder="Username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-            <div className="user">
-                <input type="password" name="confirmPassword" 
-                required=" " placeholder='Confirm Password' 
-                value={confirmPassword} onChange={(e) => setConfirmPassword( e.target.value )}
-                />
-            </div>
+        <span className="icon">
+          {open === false ? (
+            <FaEye onClick={toggle} />
+          ) : (
+            <FaEyeSlash onClick={toggle} />
+          )}
+        </span>
+        <div className="user ">
+          <input
+            type={open === false ? "password" : "text"}
+            name="password"
+            required=" "
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-            <div className='match'> { validation ()? '' :
-             'Password not matched'}</div>
+        <div className="user">
+          <input
+            type="password"
+            name="confirmPassword"
+            required=" "
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
 
+        <div className="match">
+          {" "}
+          {validation() ? "" : "Password not matched"}
+        </div>
 
-            <div className="button-form">
-              <Link to='/home'> 
-              <button className="submit" type='submit'
-               onClick={signIn}
-               >Sign up</button>
-               </Link>
-
-            </div>
+        <div className="button-form">
+          <Link to="/home">
+            <button className="submit" type="submit" onClick={signIn}>
+              Sign up
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
-    
-    </div>
-        )
+  );
 }
 
-export default Signup
+export default Signup;
