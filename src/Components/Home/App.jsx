05e703608c100cbc16a6ead700bee309ea40025f
tahreem014 <125256534+tahreem014@users.noreply.github.com>
2023-11-19 10:18:43 +0500

    delete and retaining dat after refreshing the page 10:18 11-9-23

--- src/Components/Home/App.jsx
+++ src/Components/Home/App.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { BiSolidNotepad } from "react-icons/bi";
 import { AiOutlinePlus } from "react-icons/ai";
 import { AiOutlineDelete } from "react-icons/ai";
+import { CiEdit } from "react-icons/ci";
 
 // firestore
 import { auth } from "../../config/firebase";
@@ -18,7 +19,7 @@ import { db } from "../../config/firebase";
 
 function Home() {
   const [inputFields, setInputFields] = useState([""]); // State to store input fields
-  const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(-1);
   const [todos, setTodos] = useState([]);
 
   // Logout
@@ -33,19 +34,32 @@ function Home() {
   };
 
   const input = async () => {
-    if (inputFields.length === 0 || inputFields[0] === "") {
-      return; // Do not add empty input
-    }
+    // if (inputFields.length === 0 || inputFields[0] === "") {
+    //   return; // Do not add empty input
+    // }
 
+    // await addDoc(collection(db, "todos"), {
+    //   item: inputFields[0],
+    //   id: uuidv4(),
+    //   userId: auth.currentUser.uid,
+    // });
+
+    try {
+      for (const newTodo of todos) 
+
+    // Add the new todo to Firestore
     await addDoc(collection(db, "todos"), {
-      item: inputFields[0],
-      id: uuidv4(),
+      item: newTodo.item,
+      id: newTodo.id,
       userId: auth.currentUser.uid,
     });
 
     setInputFields([...inputFields, ""]);
-    console.log("add todos");
-  };
+    console.log("add todo");
+  } catch (error) {
+    console.error("Error adding todo to Firestore:", error);
+  }
+};
 
   useEffect(() => {
     const fetchData = async () => {
@@ -64,6 +78,13 @@ function Home() {
       }
     };
 
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    } else {
+      fetchData();
+    }
+
     fetchData();
     if (auth.currentUser) {
       fetchData();
@@ -72,26 +93,57 @@ function Home() {
   }, [auth.currentUser]);
 
   // Function to add new input field
+  // const addInputField = () => {
+  //   setTodos([...todos, { id: uuidv4(), item: "" }]);
+  // };
+
   const addInputField = () => {
-    setTodos([...todos, { id: uuidv4(), item: "" }]);
+    
+      // Create a new todo with a unique ID and empty item
+      const newTodo = { id: uuidv4(), item: "" };
+  
+      // Update the local state to add the new todo
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
 
   // Function to remove an input field
   const removeInputField = async (id) => {
     try {
+      console.log("Deleting item with ID:", id);
       await deleteDoc(doc(db, "todos", id));
-  
+
       // Now, update the local state to remove the deleted item
-      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+      setTodos((prevTodos) => prevTodos.filter(todo => todo.id !== id));
+
+      localStorage.setItem(
+        "todos",
+        JSON.stringify(
+          todos.filter((todo) => todo.id !== id)
+        )
+      );
+
+      console.log("Item deleted successfully");
     } catch (error) {
       console.error("Error deleting todo from Firestore:", error);
     }
   };
 
   
-  const handleCheckboxChange = (isChecked) => {
-    setIsChecked(!isChecked);
+  // const handleCheckboxChange = (isChecked) => {
+  //   setIsChecked(!isChecked);
+  // };
+
+  const handleCheckboxChange = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isChecked: !todo.isChecked } : todo
+    );
+    setTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
   };
+
+  const editInputField = async (id) => {
+    
+  }
 
   return (
     <div>
@@ -137,14 +189,20 @@ function Home() {
               }}
             />
 
-          </div>
-        ))}
-      </div>
 
         <AiOutlineDelete
           className="delete"
-          onClick={() => removeInputField(todos.id)}
-        />
+          onClick={() => removeInputField(todo.id)}
+          />
+          
+          <CiEdit 
+          className="edit"
+          onClick={() => editInputField(todo.id)}
+          />
+
+          </div>
+        ))}
+      </div>
 
         <AiOutlinePlus
           className="plus"
